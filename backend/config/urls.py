c665fb2bfc06ab0c8ad9a5dcf0628a0dee2af164
BEYZA21 <89150497+BEYZA21@users.noreply.github.com>
2025-06-07 17:40:45 +0300

    urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,16 +3,15 @@ from django.urls import path, include
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse, HttpResponseRedirect
 
-def root_or_redirect(request):
-    # Health check (Render veya başka botlardan gelenlere 200 OK dön)
-    user_agent = request.META.get('HTTP_USER_AGENT', '')
-    if 'render' in user_agent.lower() or 'uptime' in user_agent.lower():
-        return JsonResponse({"status": "ok"})
-    # Tarayıcıdan gelenlere redirect (isteğe bağlı)
-    return HttpResponseRedirect('https://wise-pr89.onrender.com')
+# def root_or_redirect(request):
+#     # Health check (Render veya başka botlardan gelenlere 200 OK dön)
+#     user_agent = request.META.get('HTTP_USER_AGENT', '')
+#     if 'render' in user_agent.lower() or 'uptime' in user_agent.lower():
+#         return JsonResponse({"status": "ok"})
+#     # Tarayıcıdan gelenlere redirect (isteğe bağlı)
+#     return HttpResponseRedirect('https://wise-pr89.onrender.com')
 
 urlpatterns = [
-    path('', root_or_redirect),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
 ]
