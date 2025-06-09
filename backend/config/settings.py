c2b86d674c40ba2327d3143215ead7fbaae16d89
BEYZA21 <89150497+BEYZA21@users.noreply.github.com>
2025-06-09 20:47:43 +0300

    deploy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
     "http://127.0.0.1:5173", 
-    "https://wise-byn.onrender.com"
+    "https://wise-byn-9s3y.onrender.com"
 ]
 
 
